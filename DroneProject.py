abe045693efe0573b9aa2518d3bcbd0811545476
joeldick <joeldic@gmail.com>
2017-08-04 00:17:36 -0400

    Detect orange color.

--- DroneProject.py
+++ DroneProject.py
@@ -1,8 +1,11 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
 
 if __name__ == "__main__":
-    #video_capture = cv2.VideoCapture('drone_video.avi')
-    video_capture = cv2.VideoCapture('short_drone_video.avi')
+    video_capture = cv2.VideoCapture('drone_video.avi')
+    #video_capture = cv2.VideoCapture('short_drone_video.avi')
 
     # get information about video
     fps = video_capture.get(cv2.CAP_PROP_FPS)
@@ -29,9 +32,22 @@ if __name__ == "__main__":
             #if (frameCounter>54*30 and frameCounter<104*30):
             #    video_output.write(frame)
             frameCounter += 1
-            cv2.imshow('frame', frame)
 
-        if cv2.waitKey(int(1000/fps)) == 27 or cv2.getWindowProperty('frame', 0):
+            frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            lower_orange = np.array([18, 64, 50])
+            upper_orange = np.array([35, 255, 255])
+
+            mask = cv2.inRange(frame_HSV, lower_orange, upper_orange)
+            frame_masked = cv2.bitwise_and(frame, frame, mask=mask)
+
+            #plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            #plt.show()
+            cv2.imshow('frame', frame)
+            cv2.imshow('mask', mask)
+            cv2.imshow('res', frame_masked)
+
+        if cv2.waitKey(int(1000/fps)) == ord('q') or cv2.getWindowProperty('frame', 0):
             break
 
     print("number of frames played: " + str(frameCounter))
@@ -39,4 +55,4 @@ if __name__ == "__main__":
     print("length: " + str(video_length) + " seconds")
     video_capture.release()
     #video_output.release()
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
