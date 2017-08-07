9effc9c17bdf7a58117ef44e78d2099237dd8b75
joeldick <joeldic@gmail.com>
2017-08-07 02:59:18 -0400

    Tried different trackers on person, and golf ball. MIL: Too slow, but works well on person. Less well on golf ball KCF: Faster, but works only on golf ball, only when it's big Others are way too slow, or don't work at all.

--- trackers.py
+++ trackers.py
@@ -4,16 +4,25 @@ from matplotlib import pyplot as plt
 if __name__ == "__main__":
 
     # create tracker
+    # choices are "MIL", "KCF", "BOOSTING"
+    # "MEDIANFLOW", "TLD", and "GOTURN"
     tracker = cv2.Tracker_create("MIL")
 
     # open video
-    video_capture = cv2.VideoCapture("person_drone_video.avi")
+    video_capture = cv2.VideoCapture("drone_video.avi")
+    #video_capture = cv2.VideoCapture("person_drone_video.avi")
 
     # initialize tracker bounding box
-    bbox = (708, 444, 7, 7)
+    # person in person_drone_video.avi
+    #bbox = (708, 444, 7, 7)
+    # golf ball in drone_video.avi
+    bbox = (680, 408, 21, 21)
     ret, frame = video_capture.read()
     ok = tracker.init(frame, bbox)
 
+    p1 = (int(bbox[0]), int(bbox[1]))
+    p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+    cv2.rectangle(frame, p1, p2, (0, 0, 255))
     plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     plt.show()
 
